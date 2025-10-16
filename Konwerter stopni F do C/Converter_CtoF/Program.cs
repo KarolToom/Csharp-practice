c430946cfcf0e0e70d3c0c604035aa57b9975a7e
KarolToom <Karol@DESKTOP>
2025-10-16 11:30:43 +0200

    Pierwsza wersja

---
+++ Konwerter stopni F do C/Converter_CtoF/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Przeliczanie stopnie Fahrenheita na stopnie Celsjusza
+        double F, C;   
+        Console.WriteLine("Podaj temperaturę w stopniach Fahrenheita: ");
+        F =double.Parse(Console.ReadLine());
+        C= (F - 32) * 5 / 9;    
+        Console.WriteLine("Temperatura w stopniach Celsjusza wynosi: " + C);
+        Console.ReadKey();  
+    }
+}
