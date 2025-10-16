e0022bfa467136ea27cc566606c7e18c0d54d3a2
KarolToom <Karol@DESKTOP>
2025-10-16 12:14:19 +0200

    Pierwszy commit math function

---
+++ Funkcje matematyczne/MathFunction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+    class Program
+{
+    static void Main(string[] args)
+    {
+        double x, y;
+        Console.WriteLine("Podaj x (większe od 0):");
+        x= Convert.ToDouble(Console.ReadLine());
+        y = Math.PI * Math.Sqrt(Math.Abs(Math.Sin(x)) * Math.Log(x, 2.0));
+        Console.WriteLine($"y = {y}");
+        Console.ReadKey();
+    }
+}
