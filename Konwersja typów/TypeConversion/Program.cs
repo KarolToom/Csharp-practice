184e46367a346eed707ec4bb1ee3775ad862f7f2
KarolToom <Karol@DESKTOP>
2025-10-16 11:50:53 +0200

    Zadanie 2

---
+++ Konwersja typów/TypeConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{     static void Main(string[] args)
+    {
+      const int liczbaMiesiecy = 12; //
+        int liczbaSamochodow;
+        double sredniaSprzedaz;
+        Console.WriteLine("Podaj liczbe samochodow sprzedanych w ciagu roku:");
+        liczbaSamochodow = Convert.ToInt32(Console.ReadLine());  //liczbaSamochodow =- int.Parse(Console.ReadLine());
+        sredniaSprzedaz= (double)liczbaSamochodow / liczbaMiesiecy; // tutaj zmieniamy int na double aby wynik dzielenia byl w formacie zmiennoprzecinkowym
+        Console.WriteLine($"Srednia miesieczna sprzedaz samochodow wynosi: {sredniaSprzedaz}");
+        Console.ReadKey();
+
+    }
+}
